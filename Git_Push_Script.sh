5d3bfcbdcf2c6b3dfffdf180f7db57bd2a8f3253
manju <you@example.com>
2022-10-15 12:17:50 +0530

    15-October-22

--- Git_Push_Script.sh
+++ Git_Push_Script.sh
@@ -3,4 +3,4 @@ tstamp="$(date +'%d-%B-%y')"
 git add --all && git commit -m "$* ${tstamp}" && git push
 ##git push https://username:password@myrepository.biz/file.git --all
 
-git push https://manjuscsm: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@https://github.com/manjuscsm/kubernetesPracticeManju.git
+git push https://manjuscsm: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@https://github.com/manjuscsm/kubernetesPracticeManju.git
