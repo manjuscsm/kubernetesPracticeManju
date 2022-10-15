66fa4cb19688b5c65e88d2bff0008890b5ee09a2
manjuscsm <103363689+manjuscsm@users.noreply.github.com>
2022-10-15 12:58:04 +0530

    Delete Git_Push_Script.sh

--- Git_Push_Script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-tstamp="$(date +'%d-%B-%y')"
-git add --all && git commit -m "$* ${tstamp}" && git push
-##git push https://username:password@myrepository.biz/file.git --all
-
-git push https://manjuscsm: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@https://github.com/manjuscsm/kubernetesPracticeManju.git
